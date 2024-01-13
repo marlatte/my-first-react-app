687d9bf258a1ea0c4aa362d80c4ab28e8b02bb2d
MarLatte <86508134+marlatte@users.noreply.github.com>
2024-01-13 18:37:03 -0500

    Practice more tests

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Greeting, { Person } from './Greeting.jsx';
+import { Person } from './Greeting.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
