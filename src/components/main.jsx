8524dc41b451ac6e8933ca29438fb6d9a8cec0f0
MarLatte <86508134+marlatte@users.noreply.github.com>
2024-01-15 12:37:38 -0500

    Set up for Router lesson

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Person } from './Greeting.jsx';
-import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Profile from './Profile';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: 'profile',
+    element: <Profile />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Person />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
