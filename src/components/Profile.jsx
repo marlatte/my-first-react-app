35193d9e55c5a861c722fafae10571328006b29f
MarLatte <86508134+marlatte@users.noreply.github.com>
2024-01-15 20:03:25 -0500

    End of lesson explanation, assignment not finished

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,27 @@
+import { useParams } from 'react-router-dom';
+import Spinach from './Spinach';
+import Popeye from './Popeye';
+
+const DefaultProfile = () => {
+  return <p>Oh, nothing to see here!</p>;
+};
+
 const Profile = () => {
+  const { name } = useParams();
+
   return (
     <div>
       <h1>Hello from profile page!</h1>
       <p>So, how are you?</p>
+      <hr />
+      <h2>The profile visited is here:</h2>
+      {name === 'popeye' ? (
+        <Popeye />
+      ) : name === 'spinach' ? (
+        <Spinach />
+      ) : (
+        <DefaultProfile />
+      )}
     </div>
   );
 };
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import Profile from './Profile';
+import ErrorPage from './ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
   },
   {
-    path: 'profile',
+    path: 'profile/:name',
     element: <Profile />,
   },
 ]);
